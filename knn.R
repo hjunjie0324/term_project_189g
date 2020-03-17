35998b85d7d0dbc0e5796ba758868c61592e0022
w-yuan0 <61125896+w-yuan0@users.noreply.github.com>
2020-03-16 20:22:24 -0700

    Update knn.R

--- knn.R
+++ knn.R
@@ -1,20 +1,34 @@
 
 library(knnflex)
-
-udummy <- data.frame(factorToDummies(dataIn$userID,'user'))
-idummy <- data.frame(factorToDummies(dataIn$itemID,'item'))
-x <- cbind(udummy,idummy)
-num <- 2000
+num <- 3000
 xsample <- sample(1:nrow(x),num)
 xtest <- x[xsample,]
-kdist <- knn.dist(xtest)
+# 5-fold cross validation
+xtest<-xtest[sample(nrow(xtest)),]
 
-cl <- dataIn$rating[xsample]
-cltrn <- cl[1:(num*0.8)]
-cltst <- cl[(num*0.8+1):num]
+#create 5 equally size folds
+folds<-cut(seq(1,nrow(xtest)),breaks = 5,labels = FALSE)
+err <- rep(0,5)
+acc <- rep(0,5)
 
-pred <- knn.predict(1:(num*0.8),(num*0.8+1):num, cltrn, kdist, k=3)
+for(i in 1:5){
+  #segement your data by fold using the which() function
+  testIndexes <- which(folds==i,arr.ind = TRUE)
+  testData <- xtest[testIndexes, ]
+  trainData <- xtest[-testIndexes, ]
+  data <- rbind(trainData,testData)
+  kdist <- knn.dist(data)
+  cl <- dataIn$rating[xsample]
+  cltrn <- cl[-testIndexes]
+  cltst <- cl[testIndexes]
+  pred <- knn.predict(1:(num*0.8),(num*0.8+1):num, cltrn, kdist, k=i+1)
+  
+  knnout <- knn.probability(1:(num*0.8),(num*0.8+1):num, cltrn, kdist, k=i+1)
+  knnout <- data.frame(t(knnout))
+  colnames(knnout) <- c(1,2,3,4,5)
+  
+  err[i] <- mean(abs(pred-cltst))
+  preds <- round(pred)
+  acc[i] <- sum(preds==cltst)/num*5
+}
 
-knnout <- knn.probability(1:(num*0.8),(num*0.8+1):num, cltrn, kdist, k=3)
-knnout <- data.frame(t(knnout))
-colnames(knnout) <- c(1,2,3,4,5)
