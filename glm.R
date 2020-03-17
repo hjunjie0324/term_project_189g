5f761c80501cb1d0f588ed73a3cf2bafc66e6842
Ruoyu716 <56207553+Ruoyu716@users.noreply.github.com>
2020-03-16 19:36:36 -0700

    questions on return object

--- glm.R
+++ glm.R
@@ -1,78 +1,36 @@
-#source("GLM.R")
-
-library(recosystem)
 library(regtools)
 
-#readFile <- fuction(fileName){
- # file <- read.csv(fileName)
-  #colnames(file) <- c("userID", "itemID", "rating")
-  #
-  #user_mean <- tapply(file$rating, file$userID, mean)
-  #item_mean <- tapply(file$rating, file$itemID, mean)
+buildGLM <- function(dataIn,maxRating,embedMeans,specialArgs){
+  dataIn <- read.csv(dataIn)
+  colnames(dataIn) <- c("userID", "itemID", "rating")
   
-#  for(i in (1:nrow(file))){
- #   user <-file[i,2]
-  #  item <-file[i,1]
-   # um[i,1]<-user_mean[user]
-    #im[i,1]<-item_mean[item]
-  #}
+  if(embedMeans == T){
+    user_mean <- tapply(dataIn$rating, dataIn$userID, mean)
+    item_mean <- tapply(dataIn$rating, dataIn$itemID, mean)
+    
+    for(i in (1:nrow(file))){
+      user <-dataIn[i,1]
+      item <-dataIn[i,2]
+      dataIn[i,1]<-user_mean[user]
+      dataIn[i,2]<-item_mean[item]
+    }
+  }
   
-#  file$userMean = um
- # file$itemMean = im
+  #dataIn$userID <- factorToDummies(dataIn$userID,'user')
+  #dataIn$itemID <- factorToDummies(dataIn$itemID,'item')
   
-  #return(file)
-#}
-
-setwd('C:/omsi-master')
-
-#file <- readFile('songsDataset')
-
-file <- read.csv('songsDataset.csv')
-colnames(file) <- c("userID", "itemID", "rating")
-
-user_mean <- tapply(file$rating, file$userID, mean)
-item_mean <- tapply(file$rating, file$itemID, mean)
-
-#file$userID<-NA
-
-#file$itemID<-NA
-#cbind(file,userMean,itemMean)
-#file$userMean = NA
-#file$itemMean = im
-
-for(i in (1:nrow(file))){
-  user <-file[i,2]
-  item <-file[i,1]
-  um<-c(user_mean[user])
-  im<-c(item_mean[item])
+  dummyR <- as.factor(dataIn$rating)
+  dummyR <- factorToDummies(dummyR,'rating')
+  
+  fixedData <- data.frame(dataIn[1:2],dummyR)
+  
+  glmout <- glm(rating ~ userID + itemID, data = fixedData, family = binomial)
+  #glmout <- glm(rating ~ userID, data = fixedData, family = binomial)
+  #glmout <- glm(rating ~ itemID, data = fixedData, family = binomial)
+  return(glmout)
+  
 }
 
-file$userMean <- um
-file$itemMean <- im
-
-
-
-file
-
-file$userID<-as.factor(file$userID)
-file$itemID<-as.factor(file$itemID)
-file$userMean<-as.factor(file$userMean)
-#file$userID<-as.factor(file$userID)
-
-
-RFile <- file
-
-test<-sample(1:nrow(RFile),5000)
-utest<-RFile[test,]
-utrain<-RFile[-test,]
-
-glmout <- glm(rating ~ userID + itemID, data = utrain, family = binomial)
-
-pred <- predict.glm(glmout,utest,type = "prob")
-
-pred
-
-errorLM<-MAPE(pred,utest$rating)
-errorLM
-
-
+predictGlm<-function(prosFitOut,newXs){
+  pred <- predict.glm(glmout,utest,type = "prob") 
+}
