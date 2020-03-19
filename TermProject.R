eeec099f1e272cf3afbba590d7b8b4fa4d8ebb02
Ruoyu716 <56207553+Ruoyu716@users.noreply.github.com>
2020-03-18 21:54:25 -0700

    Update TermProject.R

--- TermProject.R
+++ TermProject.R
@@ -1,6 +1,39 @@
 ratingProbsFit <- function(dataIn,maxRating,preMethod,embedMeans,specialArgs){
   if(preMehtod == "logit"){
-   
+    lst <- as.list(NULL)
+  
+  if(embedMeans){
+    user_mean <- tapply(dataIn$rating, dataIn$userID, mean)
+    item_mean <- tapply(dataIn$rating, dataIn$itemID, mean)
+    
+    for(i in (1:nrow(dataIn))){
+      user <-dataIn[i,1]
+      item <-dataIn[i,2]
+      dataIn[i,1]<-user_mean[user]
+      dataIn[i,2]<-item_mean[item]
+    }
+  }
+  
+  for(i in 1: maxRating){
+    fixedRating <- as.integer(dataIn$rating == i)
+    fixedRating <- as.factor(fixedRating)
+    fixedData <- data.frame(dataIn[1:2],fixedRating)
+    
+    memory.limit(size = 160000)
+    
+    test<-sample(1:nrow(fixedData),0.05*nrow(fixedData))
+    utest<-fixedData[test,]
+    utrain<-fixedData[-test,]
+    
+    glmout <- glm(fixedRating ~ userID + itemID, data = utrain, family = binomial)
+    lst[[i]] <- glmout
+    
+  }
+  
+  
+  probsFitOut <- list(predMethod = "logit", maxRating = maxRating,lst = lst,dataIn = dataIn)
+  class(probsFitOut)<-"recProbs"
+
   }else if(preMethod == "NMF"){
     if(embedMeans){
       stop("Error: invalid embedMean for NMF\n")  
@@ -29,6 +62,12 @@ predict.recProbs <- function(probsFitOut,newXs){
   
   
   if(probsFitOut$preMehtod == "logit"){
+    preds <- NULL
+    for(i in 1: probsFitOut$maxRating){
+      pred <- predict.glm(probsFitOut$lst[[i]],newXs,type = "response")
+      preds <- cbind(preds,pred)
+  }
+    colnames(preds)<-(1:probsFitOut$maxRating)
    
   }else if(probsFitOut$preMehtod == "NMF"){
    
