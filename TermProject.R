ce94717274e008211a46277476827c4ba33baebb
Ruoyu716 <56207553+Ruoyu716@users.noreply.github.com>
2020-03-18 22:06:20 -0700

    Update TermProject.R

--- TermProject.R
+++ TermProject.R
@@ -4,36 +4,40 @@ library(regtools)
 ratingProbsFit <- function(dataIn,maxRating,preMethod,embedMeans,specialArgs){
   if(preMehtod == "logit"){
     lst <- as.list(NULL)
-  
-  if(embedMeans){
-    user_mean <- tapply(dataIn$rating, dataIn$userID, mean)
-    item_mean <- tapply(dataIn$rating, dataIn$itemID, mean)
     
-    for(i in (1:nrow(dataIn))){
-      user <-dataIn[i,1]
-      item <-dataIn[i,2]
-      dataIn[i,1]<-user_mean[user]
-      dataIn[i,2]<-item_mean[item]
+    # Check if need to replace userId and itemID by means
+    if(embedMeans){
+      user_mean <- tapply(dataIn$rating, dataIn$userID, mean)
+      item_mean <- tapply(dataIn$rating, dataIn$itemID, mean)
+
+      for(i in (1:nrow(dataIn))){
+        user <-dataIn[i,1]
+        item <-dataIn[i,2]
+        dataIn[i,1]<-user_mean[user]
+        dataIn[i,2]<-item_mean[item]
+      }
     }
-  }
-  
-  for(i in 1: maxRating){
-    fixedRating <- as.integer(dataIn$rating == i)
-    fixedRating <- as.factor(fixedRating)
-    fixedData <- data.frame(dataIn[1:2],fixedRating)
     
-    memory.limit(size = 160000)
-    
-    test<-sample(1:nrow(fixedData),0.05*nrow(fixedData))
-    utest<-fixedData[test,]
-    utrain<-fixedData[-test,]
-    
-    glmout <- glm(fixedRating ~ userID + itemID, data = utrain, family = binomial)
-    lst[[i]] <- glmout
-    
-  }
-  probsFitOut <- list(predMethod = "logit", maxRating = maxRating,lst = lst,dataIn = dataIn)
-  class(probsFitOut)<-"recProbs"
+    # build models for each single ratings
+    for(i in 1: maxRating){
+      fixedRating <- as.integer(dataIn$rating == i)
+      fixedRating <- as.factor(fixedRating)
+      fixedData <- data.frame(dataIn[1:2],fixedRating)
+
+      memory.limit(size = 160000)
+
+      test<-sample(1:nrow(fixedData),0.05*nrow(fixedData))
+      utest<-fixedData[test,]
+      utrain<-fixedData[-test,]
+
+      glmout <- glm(fixedRating ~ userID + itemID, data = utrain, family = binomial)
+      # store the single-rating-model to a list
+      lst[[i]] <- glmout
+
+    }
+    # store necessary values in a list for later use
+    probsFitOut <- list(predMethod = "logit", maxRating = maxRating,lst = lst,dataIn = dataIn)
+    class(probsFitOut)<-"recProbs" # make it to to be a class object
 
   }else if(preMethod == "NMF"){
     if(embedMeans){
