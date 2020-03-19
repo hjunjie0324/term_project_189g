6be619bef7af6a0b06679fe28553444716f8bbb0
Ruoyu716 <56207553+Ruoyu716@users.noreply.github.com>
2020-03-18 19:29:28 -0700

    Update TermProject.R

--- TermProject.R
+++ TermProject.R
@@ -1,7 +1,7 @@
 ratingProbsFit <- function(dataIn,maxRating,preMethod,embedMeans,specialArgs){
   source(./GLM.R)
   
-  probsFitOut <- switch(preMethod, "glm" = buildGLM(dataIn,maxRating,embedMeans,specialArgs),
+  probsFitOut <- switch(preMethod, "logit" = buildGLM(dataIn,maxRating,embedMeans,specialArgs),
                         "knn" = (),"NMF" = (),"CART" = ())
   return(probsFitOut)
 }
@@ -10,7 +10,17 @@ predict.recProbs <- function(probsFitOut,newXs){
 # TODO: 1. Check no new user nor new item
 #       2. Build the required output matrix, probably in subclasses.
 #       3. Parse the preMethod from probsFitOut, making list??
-  preds<-switch(predMehod, "glm" = predictGlm(probsFitOut,newXs),
+  # source: https://www.rdocumentation.org/packages/prob/versions/1.0-1/topics/setdiff
+  isNewUser = setdiff(newXs$userID,probsFitOut$dataIN$userID)
+  isNewItem = setdiff(newXs$itemID,probsFitOut$dataIN$itemID)
+  
+  if(isNewUser != 0 || isNewItem != 0){
+    # source:https://stat.ethz.ch/R-manual/R-devel/library/base/html/stop.html
+    stop("ERROR: NEW USER OR NEW ITEM\n") 
+  }
+  
+  
+  preds<-switch(predMehod, "logit" = predictGlm(probsFitOut,newXs),
                 "knn" = (),"NMF" = (),"CART" = ())
   
   return(preds)
